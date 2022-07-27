af0d2c75f974cf75b1d54f6a03d3b50c90f0cf8d
Bill Taylor <taylorbillb80@gmail.com>
2022-07-27 10:25:41 -0500

    Add files via upload
    Added simple math functions

--- calculator.py
+++ calculator.py
@@ -6,13 +6,57 @@ root.title("Calculator")
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0,columnspan=3, padx=10, pady=10)
 
+def button_equal():
+    second_number = e.get()#gets data from input field
+    e.delete(0,END)#deletes previous content input field
+    if math == "addition":
+        e.insert(0,f_num + int(second_number))
+    if math == "subtraction":
+        e.insert(0,f_num - int(second_number))
+    if math == "multiplication":
+        e.insert(0,f_num * int(second_number))
+    if math == "division":
+        e.insert(0,f_num / int(second_number))
+        
+def button_add():
+    first_number = e.get()#gets data from input field
+    global f_num
+    global math
+    math = "addition"
+    f_num = int(first_number)
+    e.delete(0,END)#deletes previous content input field        
+
 def button_clear():
-    e.delete(0,END)
+    e.delete(0,END)#deletes previous content input field
 
 def button_click(number):
-    current = e.get()#gets data from input box
-    e.delete(0,END)#deletes previous content 
+    current = e.get()#gets data from input field
+    e.delete(0,END)#deletes previous content input field
     e.insert(0,str(current) + str(number))#inserts current data plus/cancoctanate parameter passed
+
+def button_subtract():
+    first_number = e.get()#gets data from input field
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    e.delete(0,END)#deletes previous content input field
+    
+def button_multiply():
+    first_number = e.get()#gets data from input field
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = int(first_number)
+    e.delete(0,END)#deletes previous content input field
+    
+def button_divide():
+    first_number = e.get()#gets data from input field
+    global f_num
+    global math
+    math = "division"
+    f_num = int(first_number)
+    e.delete(0,END)#deletes previous content input field
 #define buttons
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1)) #lambda allows for parameter/data strings passing via ()
 button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
@@ -24,9 +68,13 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-button_add = Button(root, text="+", padx=39, pady=20, command=lambda: button_click())
-button_equal = Button(root, text="=", padx=91, pady=20, command=lambda: button_click())
-button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
+button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
+button_equal = Button(root, text="=", padx=86, pady=20, command=button_equal)
+button_clear = Button(root, text="Clear", padx=77, pady=20, command=button_clear)
+
+button_subtract = Button(root, text="-", padx=40, pady=20, command=button_subtract)
+button_multiply = Button(root, text="*", padx=40, pady=20, command=button_multiply)
+button_divide = Button(root, text="/", padx=40, pady=20, command=button_divide)
 #put buttons on screen
 
 button_1.grid(row=3, column=0)
@@ -45,6 +93,12 @@ button_0.grid(row=4, column=0)
 button_clear.grid(row=4, column=1,columnspan=2)
 button_add.grid(row=5, column=0)
 button_equal.grid(row=5, column=1, columnspan=2)
+
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
+
+
 
 
 root.mainloop()
